a2f1fd46e1d453b1e42c19b9e0008ff48f02c3f0
femascheti <fernanda.feijo@outlook.com>
2023-10-19 15:03:57 -0300

    o que é realmente novo

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-// Lista com Perguntas
 const listaPerguntas = [
-    {
-      pergunta: "Em que ano estamos?",
-      opcoes: ["2021", "2022", "2032", "2023"],
-      resposta: "2023"
-    },
-    {
-      pergunta: "Em que século estamos?",
-      opcoes: ["XX", "XXI", "XIX", "XV"],
-      resposta: "XXI"
-    },
-    {
-      pergunta: "Em que país nasceu Pelé?",
-      opcoes: ["USA", "Canada", "Brasil", "África do Sul"],
-      resposta: "Brasil"
-    },
-  ];
+  {
+    pergunta: "Em que ano estamos?",
+    opcoes: ["2021", "2022", "2032", "2023"],
+    resposta: "2023",
+  },
+  {
+    pergunta: "Em que século estamos?",
+    opcoes: ["XX", "XXI", "XIX", "XV"],
+    resposta: "XXI",
+  },
+  {
+    pergunta: "Em que país nasceu Pelé?",
+    opcoes: ["USA", "Canada", "Brasil", "África do Sul"],
+    resposta: "Brasil",
+  },
+];
 
-// Constantes e variáveis
-  let perguntaAtual = 0;
-  let pontos = 0;
-  
-  const textoPergunta = document.getElementById("perguntas");
-  const caixaOpcoes = document.getElementById("container-opcoes");
-  const resultadoTexto = document.getElementById("resultado");
-  const elementoResultado = document.getElementById("container-resultado");
-  const botaoJogarNovamente = document.getElementById("novamente-btn");
-  
+let perguntaAtual = 0;
+let pontos = 0;
+
+const textoPergunta = document.getElementById("perguntas");
+const caixaOpcoes = document.getElementById("container-opcoes");
+const resultadoTexto = document.getElementById("resultado");
+const elementoResultado = document.getElementById("container-resultado");
+const botaoJogarNovamente = document.getElementById("novamente-btn");
+
 function pergunta() {
   if (perguntaAtual < listaPerguntas.length) {
     const perguntaMomento = listaPerguntas[perguntaAtual];
@@ -38,16 +36,16 @@ function pergunta() {
   }
 }
 
+// novo: for of / lambda
 function selecionaPergunta(perguntaMomento) {
-  for (const opcao of perguntaMomento.opcoes){
-    const elementoOpcoes = document.createElement("button"); //cria botao com alternativas ficou mais fácil assim eu acho :)
+  for (const opcao of perguntaMomento.opcoes) {
+    const elementoOpcoes = document.createElement("button"); //cria botao com alternativas
     elementoOpcoes.textContent = opcao;
     elementoOpcoes.addEventListener("click", () => respostaClicada(opcao));
-    caixaOpcoes.appendChild(elementoOpcoes);  
+    caixaOpcoes.appendChild(elementoOpcoes);
   }
 }
 
-// compara a seleção com a resposta
 function respostaClicada(opcaoSelecionada) {
   const perguntaMomento = listaPerguntas[perguntaAtual];
   if (opcaoSelecionada === perguntaMomento.resposta) {
@@ -56,20 +54,18 @@ function respostaClicada(opcaoSelecionada) {
   perguntaAtual++;
   pergunta();
 }
-  
-  function mostraResultado() {
-    textoPergunta.textContent = "Acabou!!!!";
-    caixaOpcoes.textContent = ""; 
-    resultadoTexto.textContent = `Voce acertou ${pontos} de ${listaPerguntas.length}.`;
-    elementoResultado.classList.add("mostrar");
-  }
-  
-  function jogarNovamente() {
-    perguntaAtual = 0;
-    pontos = 0;
-    elementoResultado.classList.remove("mostrar"); 
-    pergunta();
-  }
-  
-  // Começa
+function mostraResultado() {
+  textoPergunta.textContent = "Acabou!!!!";
+  caixaOpcoes.textContent = "";
+  resultadoTexto.textContent = `Voce acertou ${pontos} de ${listaPerguntas.length}.`;
+  elementoResultado.classList.add("mostrar");
+}
+
+function jogarNovamente() {
+  perguntaAtual = 0;
+  pontos = 0;
+  elementoResultado.classList.remove("mostrar");
   pergunta();
+}
+
+pergunta();
